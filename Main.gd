cbd14c69497c285cee561b57a3d358167263c4e5
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-08-27 22:56:20 +0200

    make Prism Scene instead of coding it from scratch

--- Main.gd
+++ Main.gd
@@ -5,10 +5,17 @@ extends Node3D
 @onready var prisms: Node3D = $Prisms
 
 func _ready() -> void:
-	for i in prism_count:
-		var segments = i + 4
-		var prismNode = Prism.create_prism(segments)
-		prismNode .position.z = i
-		prisms.add_child(prismNode)
-		Utils.own_recursive(prismNode, self)
-		prismNode.name = "Prism " + str(segments)
+	pass
+
+
+
+
+
+
+	# for i in prism_count:
+	# 	var segments = i + 4
+	# 	var prismNode = Prism.create_prism(segments)
+	# 	prismNode .position.z = i
+	# 	prisms.add_child(prismNode)
+	# 	Utils.own_recursive(prismNode, self)
+	# 	prismNode.name = "Prism " + str(segments)
